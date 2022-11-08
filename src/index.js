cab6cba3d44038d95ccdcf700eb8d7f32e1c986e
tao.ren <rentao0524@163.com>
2022-11-08 19:32:37 +0800

    prettier config

---
+++ src/index.js
@@ -0,0 +1,27 @@
+function test(a, b) {
+  const info = {
+    'name-key': '2323',
+    name1: '232',
+    age: 2323,
+  }
+
+  const arr = [{}, {}, {}, {}]
+
+  const some = info['name1']
+
+  const { name1, age, sdsd, weew, wew } = info
+  const p = {
+    name: '23',
+  }
+
+  console.log('hello hello')
+
+  const a = '23'
+
+  const str1 = a ? '2323' : '23232'
+
+  var name = '33'
+  console.log('hello hello')
+  const str =
+    'console.log("hello hello");console.log("hello hello");console.log("hello hello");console.log("hello hello");console.log("hello hello");console.log("hello hello");console.log("hello hello");console.log("hello hello");'
+}
